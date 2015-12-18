bfb873f4f3c45c5176fae8c93db5c2d71ca153b3
moehlc <cmohl@yahoo.com>
2015-12-18 16:30:07 +0100

    added license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='permutation_test',
       version='0.1',
       description='Implementation of Fishers permutation test',
-      #url='http://github.com/storborg/funniest',
+      url='https://github.com/cmohl2013/permutation_test',
       author='Christoph Moehl',
       author_email='christoph.moehl@dzne.de',
       license='MIT',
